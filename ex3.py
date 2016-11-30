dd9a78b1b6c558c4d95e0865c9c472733d8ad336
McKaia Spangenberg <mckaia@Wards-Air.hsd1.ca.comcast.net>
2016-11-29 23:03:17 -0800

    updated exercises

--- ex3.py
+++ ex3.py
@@ -1,23 +1,23 @@
-print "I will now count my chickens:"
+print "I will now count my chickens:" # This prints the words out
 
-print "Hens", 25 + 30 / 6
-print "Roosters" , 100 - 25 *3 %4
+print "Hens", 25 + 30 / 6 # They did the math for me
+print "Roosters" , 100 - 25 *3 %4 # Math is done
 
-print "Now I will count the eggs"
+print "Now I will count the eggs" #Words are down
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # All the math is done without my brain
 
-print "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3 + 2 < 5 - 7?" # Asks a question
 
-print 3 + 2 < 5 - 7
+print 3 + 2 < 5 - 7 # this does the math
 
-print "What is 3 + 2?" , 3 + 2
-print "What is 5 - 7?" , 5 - 7
+print "What is 3 + 2?" , 3 + 2 # this tells the answer at the end
+print "What is 5 - 7?" , 5 - 7 # tells the answer
 
-print "Oh, that's why it's False"
+print "Oh, that's why it's False" # prints words
 
-print "How about some more."
+print "How about some more." #smore words
 
-print "Is it greater?" , 5> -2
+print "Is it greater?" , 5> -2 # tells weither its true or false
 print "Is it greater or equal?" , 5 >= -2
 print "Is it less or equal?" , 5 <= -2
