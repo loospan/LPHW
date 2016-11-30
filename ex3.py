0af9ad5791327a49f5c2f69196d4a90e12c65f5b
McKaia Spangenberg <mckaiaspan@gmail.com>
2016-11-30 15:15:43 -0800

    new exercises

--- ex3.py
+++ ex3.py
@@ -1,23 +1,23 @@
 print "I will now count my chickens:" # This prints the words out
 
-print "Hens", 25 + 30 / 6 # They did the math for me
-print "Roosters" , 100 - 25 *3 %4 # Math is done
+print "Hens", 25.0 + 30.0 / 6.0 # They did the math for me
+print "Roosters" , 100.0 - 25.0 *3.0 %4.0 # Math is done
 
 print "Now I will count the eggs" #Words are down
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # All the math is done without my brain
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 # All the math is done without my brain
 
-print "Is it true that 3 + 2 < 5 - 7?" # Asks a question
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?" # Asks a question
 
-print 3 + 2 < 5 - 7 # this does the math
+print 3.0 + 2.0 < 5.0 - 7.0 # this does the math
 
-print "What is 3 + 2?" , 3 + 2 # this tells the answer at the end
-print "What is 5 - 7?" , 5 - 7 # tells the answer
+print "What is 3.0 + 2.0?" , 3.0 + 2.0
+print "What is 5.0 - 7.0?" , 5.0 - 7.0 # tells the answer
 
 print "Oh, that's why it's False" # prints words
 
 print "How about some more." #smore words
 
-print "Is it greater?" , 5> -2 # tells weither its true or false
-print "Is it greater or equal?" , 5 >= -2
-print "Is it less or equal?" , 5 <= -2
+print "Is it greater?" , 5.0> -2.0 # tells weither its true or false
+print "Is it greater or equal?" , 5.0 >= -2.0
+print "Is it less or equal?" , 5.0 <= -2.0
---
+++ ex3extra.py
@@ -0,0 +1,6 @@
+print 5 + 5
+print "What is 5 + 5" , 5 + 5
+print 1 + 2 - 1
+
+print 20 + 25
+print "Is it greater or equal?" , 5 >= -2
